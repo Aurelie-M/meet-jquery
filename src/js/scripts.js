31156f6bab93dcc83f42b5a9816d81d004c2c334
Kadoshi <aurelie.m30@gmail.com>
2017-02-13 10:54:10 +0100

    Exercise part two

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -6,6 +6,21 @@
  * started at 13.02.17
  */
 
+ const fHandleTabClick = function(oEvent){
+
+ 	oEvent.preventDefault();
+ 	const $target = oEvent.currentTarget;
+
+ 	if($target.parentNode.classList.contains("active")){
+ 		return;
+ 	}
+
+ 	document.querySelector( "ul.nav-tabs li.active" ).classList.remove("active"); // enleve la classe active
+ 	$target.parentNode.classList.add("active"); // ajoute une classe active
+ 	document.querySelector(".tab-content .tab-pane.active").classList.remove("active"); // on va remettre du contenu dans l'elt actif
+ 	document.getElementById($target.getAttribute("data-tab-target")).classList.add("active");
+ };
+
 // When page loads
 window.addEventListener("load", function(){
 
@@ -13,5 +28,11 @@ window.addEventListener("load", function(){
 	Array.from(document.querySelectorAll( 'a[rel*="external"]')).forEach( function( $elt){
 
 		$elt.setAttribute("target", "_new");
+	});
+
+	// 2. tab mecanism
+	Array.from(document.querySelectorAll( 'ul.nav.nav-tabs a' )).forEach(function($elt){
+
+		$elt.addEventListener("click", fHandleTabClick);
 	});
 });
